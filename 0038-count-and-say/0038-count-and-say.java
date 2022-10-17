3b8dd7891504f00597ab9a9c588a30329a5e1ebe
Rishi Raj <72983403+RishiiRaj@users.noreply.github.com>
2022-10-17 22:09:06 +0530

    Time: 13 ms (48.65%), Space: 42.9 MB (43.21%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,31 @@
+// stupid question, just checkout the discuss section
+class Solution {
+    public String countAndSay(int n) {
+        if(n <= 0) return "-1";
+        String result = "1";
+        
+        for(int i = 1; i < n; i++) {
+            result = build(result);
+        }
+        return result;
+    }
+    
+    private String build(String result) {
+        StringBuilder builder = new StringBuilder();
+        int p = 0;
+        while(p < result.length()) {
+            char val = result.charAt(p);// current char
+            int count = 0; // current count of that char
+            
+            // till you keep finding the same char
+            while(p < result.length() && 
+              result.charAt(p) == val){
+                p ++;
+                count ++;
+            }
+            builder.append(String.valueOf(count));
+            builder.append(val);
+        }
+        return builder.toString();
+    }
+}
